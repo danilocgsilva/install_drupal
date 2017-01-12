15b93384089b51d1c69df512daf6549a70568c07
Danilo Silva <danilocgsilva@gmail.com>
2017-01-11 22:31:42 -0200

    Provides parameter to point out waht versions was needed

--- programfiles/install_drupal.sh
+++ programfiles/install_drupal.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
-wget https://ftp.drupal.org/files/projects/drupal-8.2.5.tar.gz
-tar xvzf drupal-8.2.5.tar.gz
+
+base_url=https://ftp.drupal.org/files/projects/drupal-
+# Check if parameter was provided
+if [ -z $1 ]; then
+	version=8.2.5
+else
+	version=$1
+fi
+
+url_to_download=$base_url$version.tar.gz
+downloaded_file_name=drupal-$version.tar.gz
+
+wget $url_to_download
+tar xvzf $downloaded_file_name
