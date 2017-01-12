2316b4d69f66ce48220becc3b1233ce0803ad25f
Danilo Silva <danilocgsilva@gmail.com>
2017-01-12 12:29:30 +0000

    Can verify the download utility between curl and wget. Chooses which one exists in the system, or exit noticing the user

--- programfiles/install_drupal.sh
+++ programfiles/install_drupal.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
+# Check if system have some utility do make download
+if [ `which curl 2> /dev/null` ]; then
+	do_download(){
+		curl "$url_to_download" -o "$downloaded_file_name"
+	}
+elif [ `which wget 2> /dev/null` ]; then
+	do_download(){
+		wget $url_to_download
+	}
+else
+	echo There\'s either curl or wget installed in the system. Can\'t download.
+	exit
+fi
+
+# If have, so continues
 base_url=https://ftp.drupal.org/files/projects/drupal-
 # Check if parameter was provided
 if [ -z $1 ]; then
@@ -12,6 +27,8 @@ url_to_download=$base_url$version.tar.gz
 downloaded_file_name=drupal-$version.tar.gz
 
 #wget $url_to_download
-curl "$url_to_download" -o "$downloaded_file_name"
+#curl "$url_to_download" -o "$downloaded_file_name"
+do_download
+
 tar xvzf $downloaded_file_name
 rm $downloaded_file_name
